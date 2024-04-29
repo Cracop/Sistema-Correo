1b8f6e25eb5251493ca2096b8e0b09fbd28f00fa
Cracop <rodrigoalejandrobarrera@gmail.com>
2024-04-29 13:30:24 -0600

    ya hay persistencia y los usuarios se pueden dar de alta

--- cliente/main.py
+++ cliente/main.py
@@ -1,13 +1,85 @@
 import grpc
+import sys
+import os
+sys.path.append('./grpc')
 import turbomessage_pb2
 import turbomessage_pb2_grpc
 
-def run():
-    with grpc.insecure_channel("localhost:50051") as channel:
-        stub = turbomessage_pb2_grpc.TurboMessageStub(channel)
-        respuesta = stub.nuevoUsuario(turbomessage_pb2.Usuario(usuario="lala", contrasena="lala"))
-        print(respuesta)
+class Cliente:
+    max_correos = 5
+    def __init__(self, puerto):
+        self.logeado = False
+        self.sesion = {"usuario":"","id":0}
+        self.puerto = puerto
+        self.canal = grpc.insecure_channel("localhost:"+puerto)
+        self.stub = turbomessage_pb2_grpc.TurboMessageStub(self.canal)
+
+    def limpiar_pantalla(self):
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+    def menu_principal(self):
+        self.limpiar_pantalla()
+        print(("="*10) + "Bienvenido a 'TurboMessage'"+("="*10))
+        print("1: Login")
+        print("2: Registro")
+
+        while True:
+            accion = input("#Ingresa que acción deseas realizar: ")
+
+            if accion == "1":
+                print("Login")
+            elif accion == "2":
+                self.registrarse()
+            else:
+                print("No hay tal acción")
+
+            
+    def registrarse(self):
+        
+        correcta = False
+        print("Gracias por elegir TurboMessage, por favor completa la siguiente información")
+        usuario = input("\n #Usuario: ")
+        while  not correcta:
+            # usuario = input("\n Usuario: ")
+            passwd1 = input(" #Contraseña: ")
+            passwd2 = input(" #Confirma tu contraseña ")
+
+            if passwd1 == passwd2:
+                correcta = True
+            else:
+                print("#Las contraseñas no coinciden, por favor intentalo de nuevo")
+
+        respuesta = self.stub.nuevoUsuario(turbomessage_pb2.Usuario(usuario=usuario, contrasena=passwd1))
+        if respuesta.success:
+            print("Registro completado con exito")
+            print("Procediendo a logearte directo")
+            self.sesion["id"]=int(respuesta.mensaje)
+            self.sesion["usuario"]=usuario
+            self.bandeja()
+        else:
+            print("ERROR: Ya existe un usuario con ese nombre")
+
+    def login(self):
+        pass
+            
+
+    def bandeja(self):
+        print("Bienvenid@: "+self.sesion["usuario"]+str(self.sesion["id"]))
+
+
+
+# def run():
+#     with grpc.insecure_channel("localhost:50051") as channel:
+#         stub = turbomessage_pb2_grpc.TurboMessageStub(channel)
+#         respuesta = stub.nuevoUsuario(turbomessage_pb2.Usuario(usuario="lala", contrasena="lala"))
+#         print(respuesta)
 
 
 if __name__ == '__main__':
-    run()
+    # puerto = input("¿A que puerto desea conectarse?\n Puerto: ")
+    puerto = "50051"
+    cliente = Cliente(puerto)
+    cliente.menu_principal()
--- servidor/main.go
+++ servidor/main.go
@@ -2,29 +2,135 @@ package main
 
 import (
 	context "context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"os"
 	pb "servidor/proto"
+	"strconv"
+	"sync"
 
 	"google.golang.org/grpc"
 )
 
 var (
-	port = flag.Int("port", 50051, "The server port")
+	port            = flag.Int("port", 50051, "The server port")
+	filenameUsers   = "db_users.json"
+	filenameCorreos = "db_correos.json"
+	usersMap        = make(map[int]Usuario)
+	usersLock       sync.Mutex
 )
 
-// server is used to implement helloworld.GreeterServer.
-type server struct {
+type Usuario struct {
+	User   string `json:"usuario"`
+	Passwd string `json:"passwd"`
+}
+
+type Server struct {
 	pb.TurboMessageServer
 }
 
 // El nombre de la función lo busco en turbomessage_grpc.pb
-func (s *server) NuevoUsuario(ctx context.Context, in *pb.Usuario) (*pb.Status, error) {
+func (s *Server) NuevoUsuario(ctx context.Context, in *pb.Usuario) (*pb.Status, error) {
 	fmt.Print(in.Usuario)
-	return &pb.Status{Success: &[]bool{true}[0], Mensaje: &[]string{"John"}[0]}, nil
+	usersLock.Lock()
+	//LIFO
+	defer usersLock.Unlock()
+	defer reloadDBs()
+	var found = false
+	id := rand.Intn(101) + 1
+	// Check if the map is empty
+	if len(usersMap) == 0 {
+
+		usersMap[id] = Usuario{User: *in.Usuario, Passwd: *in.Contrasena}
+		return &pb.Status{Success: &[]bool{true}[0], Mensaje: &[]string{strconv.Itoa(id)}[0]}, nil
+	} else {
+		for _, person := range usersMap {
+			if person.User == *in.Usuario {
+				found = true
+				break
+			}
+
+		}
+		if found {
+			return &pb.Status{Success: &[]bool{false}[0], Mensaje: &[]string{"0"}[0]}, nil
+		} else {
+			for {
+				id = rand.Intn(101) + 1
+				if _, exists := usersMap[id]; !exists {
+					// If the id does not exist in the map, break out of the loop
+					break
+				}
+			}
+			usersMap[id] = Usuario{User: *in.Usuario, Passwd: *in.Contrasena}
+			return &pb.Status{Success: &[]bool{true}[0], Mensaje: &[]string{strconv.Itoa(id)}[0]}, nil
+		}
+
+	}
+
 }
+
+func reloadDBs() {
+
+	jsonData, err := json.Marshal(usersMap)
+	if err != nil {
+		return
+	}
+	os.WriteFile(filenameUsers, jsonData, 0644)
+
+	data, err := os.ReadFile(filenameUsers)
+	if err != nil {
+		return
+	}
+
+	if err := json.Unmarshal(data, &usersMap); err != nil {
+		return
+	}
+}
+
+func init() {
+	_, err := os.Stat(filenameUsers)
+	if os.IsNotExist(err) {
+		// File doesn't exist, create it
+		file1, err := os.Create(filenameUsers)
+
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer file1.Close()
+		fmt.Println("File created:", filenameUsers)
+	}
+
+	_, err = os.Stat(filenameCorreos)
+	if os.IsNotExist(err) {
+		// File doesn't exist, create it
+		file1, err := os.Create(filenameCorreos)
+
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer file1.Close()
+		fmt.Println("File created:", filenameCorreos)
+	}
+
+	data, err := os.ReadFile(filenameUsers)
+	if err != nil {
+		return
+	}
+
+	if err := json.Unmarshal(data, &usersMap); err != nil {
+		return
+	}
+}
+
+// func startServer() {
+
+// }
 
 func main() {
 	flag.Parse()
@@ -33,7 +139,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterTurboMessageServer(s, &server{})
+	pb.RegisterTurboMessageServer(s, &Server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
